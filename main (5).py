e8a7bdefbfd0e1d01e0bf443481e018e05df53d4
Vaishudoss <144469948+Vaishudoss@users.noreply.github.com>
2023-09-22 19:19:27 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,12 @@
+def linearsearchproduct(productList, targetproduct ):
+  indices=[]
+  for indix, product in enumerate(productList):
+    if product ==Targetproduct:
+      indices.append(index)
+
+  return indices
+  products=["shoes,boot,loafer, shoes,sandal, shoes"]
+  target1 ="shoes"
+  target2 ="apple"
+  result =linearsearchproduct(products,target2)
+  print (result)
